280f4eedd9d606d7640d30aba684d22b721a4cce
AndreaMinelli <minelliandrea94@gmail.com>
2023-01-20 15:56:24 +0100

    Creata AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,19 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <h3>-- Content goes here --</h3>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  line-height: 100px;
+  background-color: black;
+  color: white;
+}
+</style>
