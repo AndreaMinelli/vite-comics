7bc62c11e6744e37b7e22b0f2f6fe228f8ae306b
AndreaMinelli <minelliandrea94@gmail.com>
2023-01-23 15:36:53 +0100

    Creato modulo MainCard

---
+++ src/components/main-child/MainCard.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  props: {
+    comic: Object,
+  },
+};
+</script>
+
+<template>
+  <figure>
+    <img :src="comic.thumb" :alt="comic.series" />
+  </figure>
+  <p>{{ comic.series }}</p>
+</template>
+
+<style lang="scss" scoped>
+.comic-card {
+  padding: 10px;
+
+  figure {
+    width: 130px;
+    height: 130px;
+    overflow: hidden;
+    cursor: pointer;
+  }
+
+  p {
+    text-transform: uppercase;
+    margin: 15px 0;
+    font-size: 11px;
+    cursor: pointer;
+  }
+}
+</style>
